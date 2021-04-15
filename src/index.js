07143bdd189049cfeac441375bbd615d12f96ea1
dessanhemrayev <dessanhemrayev@gmail.com>
2021-04-15 13:35:03 +0300

    react-second-app

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from "react-dom";
-
-import App from "./App";
-import 'bootstrap/dist/css/bootstrap.css';
-import Counters from './components/counters.jsx'
-
-ReactDOM.render(<Counters />, document.getElementById('root'));
